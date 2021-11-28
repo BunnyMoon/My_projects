365ec689bf5d74ceff59ff34c55ae44dad81ebf3
BunnyMoon <tocia2000@gmail.com>
2021-11-28 14:21:36 +0300

    My HW to L7

---
+++ CW_1.py
@@ -0,0 +1,4 @@
+# When data is not
+# what it should be
+value = int(input('Enter a natural number: '))
+print('The reciprocal of'), value  , 'is', 1/value)
---
+++ CW_2.py
@@ -0,0 +1,6 @@
+# The try-except branch
+try:
+    value = int(input('Enter a natural number: '))
+    print('The reciprocal of', value, 'is', 1/value)
+except:
+    print('I do not know what to do.')
